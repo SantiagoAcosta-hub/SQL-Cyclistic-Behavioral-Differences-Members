8c8fb833a77e13d58979b025c8049c32f72a1395
Santiago Acosta <a.antibohorquez@gmail.com>
2026-04-03 15:50:06 -0300

    2. Relevant data - Cyclistic.sql

--- 2. Relevant data - Cyclistic.sql
+++ 2. Relevant data - Cyclistic.sql
@@ -1,16 +1,17 @@
--- For this analysis we wont need the latitud or longitudes so we can just drop those
--- Creating a view instead of a tableso we dont store physical data. 
-CREATE OR REPLACE VIEW `drift-project-1.Bike_trips.Dec2024_to_june2025_clean`
+-- we wont need the latitud or longitudes,ride id and station id
+-- Because the marketing is digital so we dont care about the stations much
+-- Creating a view instead of a table so we dont store physical data. 
+-- This is de data well actually work with
+CREATE OR REPLACE VIEW `drift-project-1.Bike_trips.relevant_data`
 AS
 SELECT
-ride_id,
 rideable_type,
 started_at,
 ended_at,
-start_station_name,
-start_station_id,
-end_station_name,
-end_station_id,
 member_casual
 FROM
 `drift-project-1.Bike_trips.Dec2024_to_june2025`
+
+
+SELECT *
+FROM `drift-project-1.Bike_trips.relevant_data`
